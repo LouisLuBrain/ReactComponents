9f7f15774707a40e050c9a076442a520419040f8
LouisLuBrain <1394853556@qq.com>
2019-12-19 10:40:52 +0800

    topbar init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import TopBar from './components/TopBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopBar
+        sectionList={[
+          {
+            url:'',
+            icon:'user',
+            label:'Users Manage'
+          },
+          {
+            url:'',
+            icon:'tasks',
+            label:'Tasks Manage'
+          },
+          {
+            url:'',
+            icon:'users',
+            label:'Member Manage'
+          },
+          {
+            url:'',
+            icon:'sitemap',
+            label:'Organization Manage'
+          }
+          ]}></TopBar>
     </div>
   );
 }
---
+++ src/components/TopBar.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './TopBar.css';
+class TopBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: props.username||"Guset",
+      sectionList: props.sectionList
+        ? props.sectionList.map((item) => {
+            item.active = false;
+            return item;
+          })
+        : []
+    };
+    
+    this.handleClick = (e) => {
+      let idx = e.target.getAttribute('index');
+      
+      const sectionList = this.state.sectionList.map((item) => {
+        item.active=false;
+        return item;
+      });
+
+      sectionList[idx].active = true;
+      this.setState({sectionList: sectionList});
+    };
+  }
+
+  render() {
+    return (
+      <div className="topbar">
+        <div className="topbar-logo">
+          <img className="topbar-logo-img" alt="pic missing" url="" />
+      </div>
+        <div className="topbar-list">
+          {this.state.sectionList.map((item,index)=>
+            (
+              <TopbarItem
+                url={item.url||''}
+                active={item.active||false}
+                icon={item.icon}
+                key={index}
+                index={index}
+                handleClick={this.handleClick}
+              >{item.label||item}</TopbarItem>
+            )
+          )}
+        </div>
+        <div className="topbar-user">
+          <div className="topbar-user-avatar">
+            <img className="topbar-user-avatar-img" alt="pic missing" url=""/>
+          </div>
+          <span className="topbar-user-name">{this.state.username}</span>
+        </div>
+      </div>
+    );
+  }
+
+}
+function TopbarItem(props) {
+  return (
+    <div
+      onClick={props.handleClick}
+      index={props.index}
+      className={`topbar-item ${props.active?'active':''}`}>
+    <i className={`fa fa-${props.icon} topbar-item-icon`}></i>
+    {props.children}
+    </div>);
+}
+export default TopBar; 
