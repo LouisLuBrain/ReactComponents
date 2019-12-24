53bc78f345082b4455c8173857f1fca28ab5d554
LouisLuBrain <1394853556@qq.com>
2019-12-24 13:37:32 +0800

    new topbar component (hook version)

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,74 +1,54 @@
-import React from 'react';
+import React, { useState, useContext, useReducer } from 'react';
 import './TopBar.css';
-/**
- * topbar component
- * @class TopBar
- * @extends {React.Component}
- */
-class TopBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: props.username||"Guset",
-      sectionList: props.sectionList
-        ? props.sectionList.map((item) => {
-            item.active = false;
-            return item;
-          })
-        : []
-    };
-    
-    this.handleClick = (e) => {
-      // idx: the item which be clicked
-      let idx = e.target.getAttribute('index');
-      // reset the flags
-      const sectionList = this.state.sectionList.map((item) => {
-        item.active=false;
-        return item;
-      });
-      sectionList[idx].active = true;
 
-      this.setState({sectionList: sectionList});
-    };
-  }
-
-  render() {
-    return (
-      <div className="topbar">
-        <div className="topbar-logo iconfont icon-logo"></div>
-        <div className="topbar-list">
-          {this.state.sectionList.map((item,index)=>
+function TopBar(props) {
+  const [selectedID, dispatch] = useReducer(reducer, 0);
+  const [username] = useState(props.username||'Guset');
+  return (
+    <div className="topbar">
+      <div className="topbar-logo iconfont icon-logo"></div>
+      <div className="topbar-list">
+        <SelectContext.Provider value={{selectedID, dispatch}}>
+          {props.sectionList.map((item,index)=>
             (
               <TopbarItem
                 url={item.url||''}
-                active={item.active||false}
                 icon={item.icon}
                 key={index}
-                index={index}
-                handleClick={this.handleClick}
-              >{item.label||item}</TopbarItem>
+                index={index}>
+                  {item.label||item}
+              </TopbarItem>
             )
           )}
-        </div>
-        <div className="topbar-user">
-          <div className="topbar-user-avatar">
-            <img className="topbar-user-avatar-img" alt="pic missing" url=""/>
-          </div>
-          <span className="topbar-user-name">{this.state.username}</span>
-        </div>
+        </SelectContext.Provider>
       </div>
-    );
-  }
-
+      <div className="topbar-user">
+        <div className="topbar-user-avatar">
+          <img className="topbar-user-avatar-img" alt="pic missing" url=""/>
+        </div>
+        <span className="topbar-user-name">{username}</span>
+      </div>
+    </div>
+  );
 }
+// topbar item
 function TopbarItem(props) {
+  const {selectedID, dispatch} = useContext(SelectContext);
+  const index = props.index;
+
   return (
     <div
-      onClick={props.handleClick}
-      index={props.index}
-      className={`topbar-item ${props.active?'active':''}`}>
+      onClick={()=> dispatch(index)}
+      className={`topbar-item ${props.index === selectedID ? 'active':''}`}>
       <i className={`fa fa-${props.icon} topbar-item-icon`}></i>
       {props.children}
     </div>);
+}
+// context
+const SelectContext = React.createContext(null)
+
+// topbar item hook
+function reducer(state, action) {
+  return action;
 }
 export default TopBar; 
