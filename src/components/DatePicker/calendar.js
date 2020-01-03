08f858a32d79545644ba41ace841c55f494d2d44
LouisLuBrain <1394853556@qq.com>
2020-01-03 16:00:13 +0800

    calendar init

--- src/components/DatePicker/calendar.js
+++ src/components/DatePicker/calendar.js
@@ -2,17 +2,35 @@ const day = 31;
 
 const monthList = [0, -2, 0, -1, 0, -1, 0, 0, -1, 0, -1, 0]
 
-function getDayOfMonth(date) {
-  let year = date.year
-  let month = date.month
+function getDayOfMonth(year, month) {
   // 闰年二月
-  if (month === 1 && !year % 4) {
+  if (month === 1 && (year % 400 === 0) || (year % 100 !== 0 && year % 4 === 0)) {
     return day + monthList[month] + 1
   }
   // 平年、普通月
   return day + monthList[month]
 }
 
+function getWeekDay(date) {
+  let year = date.year
+  let month = date.month
+  return new Date(year, month, 1).getDay()
+}
+
+function getPreDayOfMonth(year, month) {
+  return month === 0 
+    ? getDayOfMonth(year - 1, 11) 
+    : getDayOfMonth(year, month - 1)
+}
+function getNextDayOfMonth(year, month) {
+  return month === 11 
+    ? getDayOfMonth(year + 1, 0) 
+    : getDayOfMonth(year, month + 1)
+}
+
 export default {
-  getDayOfMonth: getDayOfMonth
+  getDayOfMonth: getDayOfMonth,
+  getPreDayOfMonth: getPreDayOfMonth,
+  getNextDayOfMonth: getNextDayOfMonth,
+  getWeekDay: getWeekDay
 }
--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -3,11 +3,11 @@ import calendar from './calendar'
 
 function DatePicker(props) {
   const [ days, setDays ] = useState(31)
-  const [ isShow, setIsshow ] = useState(false)
+  const [ isShow, setIsshow ] = useState(true)
 
   const handleChange = (e) => {
     const value = e.target.value
-    setDays(calendar.getDayOfMonth(new Date()))
+    setDays(calendar.getDayOfMonth(new Date().getFullYear(), new Date().getMonth()))
   }
   const showPanel = () => {
     setIsshow(!isShow)
@@ -15,7 +15,7 @@ function DatePicker(props) {
   return (
     <div>
       <DateSpan showPanel={showPanel}/>
-      <PickPanel showPanel={showPanel} isShow={isShow}/>
+      <Calendar showPanel={showPanel} isShow={isShow}/>
     </div>
   )
 }
@@ -28,16 +28,52 @@ function DateSpan(props) {
   )
 }
 // calendar
-function PickPanel(props) {
-  const [ date, setDate ] = useState(Ddate(new Date()))
+function Calendar(props) {
+  const [ date, setDate ] = useState(new Date())
 
+  const numOfDays = calendar.getDayOfMonth(date.getFullYear(), date.getMonth())
 
 
   return (
     <div
-     className={`mx-1 w-64 ${props.isShow?'h-64 opacity-1':'h-0 opacity-0'} rounded border-gray-200 border-1 shadow-lg absolute left-0 top-0 bg-white t-linear t-dur-1`}></div>
+      className={`mx-1 w-64 ${props.isShow?'h-64 opacity-1':'h-0 opacity-0'} rounded border-gray-200 border-1 shadow-lg absolute left-0 top-0 bg-white t-linear t-dur-1`}>
+      <div className="m-2">
+        <div></div>
+        <div className="flex justify-around mb-1 text-center">
+          <div className="w-auto text-red-600">Sun</div>
+          <div className="w-auto">Mon</div>
+          <div className="w-auto">Tue</div>
+          <div className="w-auto">Wed</div>
+          <div className="w-auto">Thr</div>
+          <div className="w-auto">Fri</div>
+          <div className="w-auto text-red-600">Sat</div>
+        </div>
+        <PickPanel numOfDays={numOfDays} today={date}></PickPanel>
+      </div>
+    </div>
   )
 }
+
+function PickPanel(props) {
+  const d = [0,0,0,0,0,0,0]
+  return (
+    <div>
+      {d.map(() => (
+        <div className="flex justify-around my-1 text-center">
+          <div className="w-auto">1</div>
+          <div className="w-auto">2</div>
+          <div className="w-auto">3</div>
+          <div className="w-auto">4</div>
+          <div className="w-auto">5</div>
+          <div className="w-auto">6</div>
+          <div className="w-auto">7</div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+// trans Date object
 const Ddate = (date) => {
   return {
     year: date.getFullYear(),
